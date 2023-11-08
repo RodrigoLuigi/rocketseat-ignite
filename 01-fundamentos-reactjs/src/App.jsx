fcd2063725634c55a63f5ba1fb9cf7e5798eaf9f
Rodrigo Luigi <digaor@gmail.com>
2023-11-08 20:16:21 -0300

    feat: create Post component

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,6 +1,9 @@
-import { Header } from './components/Header'
 import { Sidebar } from './components/Sidebar'
+import { Header } from './components/Header'
+import { Post } from './components/Post'
+
 import styles from './App.module.css'
+
 import './global.css'
 
 export function App() {
@@ -11,15 +14,7 @@ export function App() {
 			<div className={styles.wrapper}>
 				<Sidebar />
 				<main>
-					<div>
-						<strong>Rodrigo</strong>{' '}
-						<p>
-							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime
-							fuga nemo eius nihil accusamus laudantium? Repudiandae est, error,
-							quas natus assumenda impedit quia placeat neque, ut et repellendus
-							dignissimos sed!
-						</p>
-					</div>
+					<Post />
 				</main>
 			</div>
 		</div>
