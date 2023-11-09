5c70b73d5b31b1e4f5a39ddb216b1d25048f72e4
Rodrigo Luigi <digaor@gmail.com>
2023-11-09 12:15:49 -0300

    Update directory

---
+++ 01-fundamentos-reactjs/src/components/Sidebar/index.jsx
@@ -0,0 +1,29 @@
+import { PencilLine } from 'phosphor-react'
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+	return (
+		<aside className={styles.sidebar}>
+			<img
+				className={styles.cover}
+				src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?q=40&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+			/>
+
+			<div className={styles.profile}>
+				<img
+					className={styles.avatar}
+					src="https://github.com/RodrigoLuigi.png"
+				/>
+				<strong>Rodrigo Luigi</strong>
+				<span>Web Developer</span>
+			</div>
+
+			<footer>
+				<a href="#">
+					<PencilLine size={20} />
+					Editar seu perfil
+				</a>
+			</footer>
+		</aside>
+	)
+}
