f1a3a11ba8c2df92ca644167f21e9df88d14da2c
Rodrigo Luigi <digaor@gmail.com>
2023-11-20 20:18:28 -0300

    add styled-components /  create theme and global styles

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,7 +1,20 @@
+import { ThemeProvider } from 'styled-components'
+
+import { Button } from './components/Button'
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/theme/default'
+
 export function App() {
 	return (
-		<>
-			<p>Click on the Vite and React logos to learn more</p>
-		</>
+		<ThemeProvider theme={defaultTheme}>
+			<Button variant="primary" />
+			<Button variant="secondary" />
+			<Button variant="success" />
+			<Button variant="danger" />
+			<Button />
+			<h1>xx</h1>
+			<GlobalStyle />
+		</ThemeProvider>
 	)
 }
